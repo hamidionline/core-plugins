7ca35d39355b29fedc57abfd2d779a1a01557636
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-21 20:21:07 -0700

    Add files via upload

--- deepl/language/en-GB.php
+++ deepl/language/en-GB.php
@@ -1,1 +1,19 @@
-  
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_DEEPL_TITLE',"DeepL translations");
+jr_define('_DEEPL_DESCRIPTION',"DeepL is a reasonably priced in-line translation service that you can use to translate Jomres language strings automatically. These translations, once received from the DeepL service, are stored locally and you can use the Label Translations feature to refine them. This significantly speeds up the process of making your website work with multiple languages.");
+jr_define('_DEEPL_API_KEY_SETTING',"DeepL Authentication Key");
+jr_define('_DEEPL_API_KEY_SETTING_DESC',"You will need a valid API Key from <a href='' target='_blank'>DeepL.</a> Once you have subscribed, you will find the authentication key in <a href='https://www.deepl.com/subscription.html' target='_blank'>your account.</a>  After you have saved the key in Jomres your site may slow down for a little while as there will be a lot of translations that need to be pulled from the DeepL service. Be patient and reload the page a few times. Afterwards you can finetune the customisations in the lable translations page.");
---
+++ deepl/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_DEEPL_TITLE',"Traductions avec DeepL");
+jr_define('_DEEPL_DESCRIPTION',"DeepL est un service de traduction en ligne à prix raisonnable que vous pouvez utiliser pour traduire automatiquement les chaînes de caractères de Jomres. Ces traductions, une fois reçues du service DeepL, sont stockées localement et vous pouvez utiliser la fonctionnalité Étiquettes de traduction pour les affiner. Cela accélère considérablement le processus permettant à votre site Web de fonctionner avec plusieurs langues.");
+jr_define('_DEEPL_API_KEY_SETTING',"Clef d'authentification de DeepL");
+jr_define('_DEEPL_API_KEY_SETTING_DESC',"Vous aurez besoin d'une clé d'API valide de DeepL. Une fois abonné, vous trouverez la clé d'authentification dans <a href='https://www.deepl.com/subscription.html' target='_blank'> votre compte. </a> Une fois la clé enregistrée dans Jomres, votre site risque de ralentir pendant un certain temps, car de nombreuses traductions devront être extraites. du service DeepL. Soyez patient et rechargez la page plusieurs fois. Ensuite, vous pouvez affiner les personnalisations dans la page des traductions des étiquettes.");
