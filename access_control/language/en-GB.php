7569f833339c4ac1193dce4bc12b5a722f176d7b
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-21 19:25:51 -0700

    Create en-GB.php

---
+++ access_control/language/en-GB.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2010 Vince Wooll
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_ACCESS_CONTROL', 'Access Control' );
+jr_define( '_JOMRES_ACCESS_CONTROL_DESC', 'This feature allows you to set a minimum access level required by users to access specific tasks.' );
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_DEFAULT', 'Default');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_ANYBODY', 'Anybody');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_REGISTERED', 'Registered');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_MANAGER', 'Manager');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_SUPERMANAGER', 'Super Manager');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_NOBODY', 'Nobody');
+jr_define('_JOMRES_ACCESS_CONTROL_LEVELS_RECEPTIONIST', 'Receptionist');
