a25c30de0d3d4bebcf6ce4fd26f5fa794e52dc14
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 05:23:28 -0700

    Add files via upload

---
+++ je_mapview/language/en-GB.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_MAPVIEW_TITLE',"Property List Map View");
+jr_define('_JRPORTAL_JE_MAPVIEW_HWIDTH',"Map width (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HHEIGHT',"Map height (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HOVERRIDE_PROPERTYLIST',"Override default jomres propertylist? ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HINFOICON',"Other events/attractions marker icon ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPOPUP_WIDTH',"Popup width (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPROPERTY_IMGWIDTH',"Image width (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPROPERTY_IMGHEIGHT',"Image height (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HSHOW_DESCRIPTION',"Display property description? (only for rentals popups)");
+jr_define('_JRPORTAL_JE_MAPVIEW_HTRIM_DESCRIPTION',"Trim property description? (only for rentals popups)");
+jr_define('_JRPORTAL_JE_MAPVIEW_HTRIM_VALUE',"Trim property description after (chars) (only for rentals popups)");
+jr_define('_JRPORTAL_JE_MAPVIEW_MAPVIEW',"Map view");
+jr_define('_JRPORTAL_JE_MAPVIEW_GROUPMARKERS',"Group markers");
---
+++ je_mapview/language/fr-FR.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_MAPVIEW_TITLE',"Liste des établissements sur une carte");
+jr_define('_JRPORTAL_JE_MAPVIEW_HWIDTH',"Largeur de la carte (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HHEIGHT',"Hauteur de la carte (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HOVERRIDE_PROPERTYLIST',"Ignorer les établissements par défaut de Jomres ?");
+jr_define('_JRPORTAL_JE_MAPVIEW_HINFOICON',"Icônes des marqueurs d'autres événements/attractions sur la carte ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPOPUP_WIDTH',"Largeur de la popup (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPROPERTY_IMGWIDTH',"Largeur de l'image (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HPROPERTY_IMGHEIGHT',"Hauteur de l'image (px) ");
+jr_define('_JRPORTAL_JE_MAPVIEW_HSHOW_DESCRIPTION',"Afficher la description de l'établissement ? (uniquement pour les popups des locations)");
+jr_define('_JRPORTAL_JE_MAPVIEW_HTRIM_DESCRIPTION',"Réduire la description de l'établissement ? (uniquement pour les popups des locations)");
+jr_define('_JRPORTAL_JE_MAPVIEW_HTRIM_VALUE',"Réduire la description de l'établissement après x caractères (uniquement pour les popups des locations)");
+jr_define('_JRPORTAL_JE_MAPVIEW_MAPVIEW',"Vue de la carte");
+jr_define('_JRPORTAL_JE_MAPVIEW_GROUPMARKERS',"Marqueurs de groupe");
