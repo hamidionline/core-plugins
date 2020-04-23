528a515b0e2300c2dbf060e1bebc8d72063392a0
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 19:22:18 -0700

    Add files via upload

---
+++ gmap_driving_directions/language/fr-FR.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 26/06/2019 - https://www.valtari.fr
+* @copyright 2009-2010 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_GMAPS_DD_YES',"Oui");
+jr_define('_JRPORTAL_GMAPS_DD_NO',"Non");
+jr_define('_GOOGLE_ADDRESS',"Adresse");
+jr_define('_GOOGLE_INPUT_FIELDSET_POSTALCODE',"Code Postal");
+jr_define('_GOOGLE_INPUT_FIELDSET_TOWN',"Ville");
+jr_define('_GOOGLE_INITIALIZE_ERROR1',"Nombre maximum de 9 points de cheminement atteints");
+jr_define('_GOOGLE_INITIALIZE_ERROR2',"Google n'a pas pu calculer d'itinéraire avec les options sélectionnées");
+jr_define('_GOOGLE_DIRECTION_PRINT',"Imprimer l'itinéraire");
+jr_define('_GOOGLE_ROUTEPLANNING',"Planifiez votre itinéraire pour ");
+jr_define('_GOOGLE_ROUTEPLANNING_HYOURLOCATION',"Votre position (point de départ)");
+jr_define('_GOOGLE_ROUTEPLANNING_HROUTEOPTIONS',"Options d'itinéraire");
+jr_define('_GOOGLE_ROUTEPLANNING_HOPTIMIZEFOR',"Choisir le type de transport : ");
+jr_define('_GOOGLE_ROUTEPLANNING_HDRIVING',"En voiture");
+jr_define('_GOOGLE_ROUTEPLANNING_HWALKING',"À pieds");
+jr_define('_GOOGLE_ROUTEPLANNING_HCYCLING',"En vélo");
+jr_define('_GOOGLE_ROUTEPLANNING_HHIGHWAYS',"Éviter les autoroutes");
+jr_define('_GOOGLE_ROUTEPLANNING_HTOLLS',"Éviter les péages");
+jr_define('_GOOGLE_SELECT_BUTTON',"Voir l'itinéraire");
+jr_define('_GOOGLE_SELECT_RESETMAP',"Réinitialiser la carte");
+jr_define('_GOOGLE_ROUTEPLANNING_INSTRUCTIONS',"Votre destination (point d'arrivée de l'itinéraire) est déjà marquée sur la carte. Veuillez entrer votre position et choisir les options de votre itinéraire pour obtenir les directions jusqu'au point de destination. Vous pouvez également cliquer sur la carte pour choisir jusqu'à 9 points de cheminement de votre point de départ au point de destination.");
