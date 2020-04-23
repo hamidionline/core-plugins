897476e7f9620d73e5e9b0d699a84588201a76cb
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 05:31:23 -0700

    Add files via upload

---
+++ je_overview/language/en-GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( "_JOMRES_HOVERVIEW", "Overview" );
+jr_define( "_JOMRES_HOVERVIEW_CHECKINS", "Arriving today" );
+jr_define( "_JOMRES_HOVERVIEW_CHECKOUTS", "Departing today" );
+jr_define( "_JOMRES_HOVERVIEW_CURRENT_RESIDENTS", "Current residents" );
+jr_define( "_JOMRES_HOVERVIEW_COMMISSION_INVOICES", "Pending Commission Invoices" );
---
+++ je_overview/language/fr-FR.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( "_JOMRES_HOVERVIEW", "Vue d'ensemble" );
+jr_define( "_JOMRES_HOVERVIEW_CHECKINS", "Arrivés du jour" );
+jr_define( "_JOMRES_HOVERVIEW_CHECKOUTS", "Départ du jour" );
+jr_define( "_JOMRES_HOVERVIEW_CURRENT_RESIDENTS", "Résidents actuels" );
+jr_define( "_JOMRES_HOVERVIEW_COMMISSION_INVOICES", "Factures de commission en attente" );
