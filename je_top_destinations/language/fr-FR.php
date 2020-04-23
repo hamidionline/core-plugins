fe246470349894cb3be1bd0476ae054b0a2c565f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 05:37:57 -0700

    Add files via upload

---
+++ je_top_destinations/language/fr-FR.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_TOP_DESTINATIONS_HPROPERTIES',"Établissements");
+jr_define('_JRPORTAL_JE_TOP_DESTINATIONS_HALLDESTINATIONS',"Toutes les destinations");
+jr_define('_JRPORTAL_JE_TOP_DESTINATIONS_BROWSEALLDESTINATIONS',"Parcourir toutes les destinations");
