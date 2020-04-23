42817f6173e638dcd3ea4a4f131353c8a427581f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 05:16:30 -0700

    Add files via upload

---
+++ je_iframe_content_tab/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_TITLE',"Intégration d'Iframe");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_TITLE_TAB',"Code de l' Iframe");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_ENABLED_YESNO',"Activée ? ");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_ENABLED_YES',"Oui");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_ENABLED_NO',"Non");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_WIDTH',"Largeur de l'Iframe  ");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_HEIGHT',"Hauteur de l'Iframe ");
+jr_define('_JRPORTAL_JE_IFRAME_CONTENT_TAB_INSTRUCTIONS_FRONTEND',"URL de l'Iframe ");
