b60443e4927feb56d839e3195503f995338f3698
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 00:30:24 -0700

    Add files via upload

---
+++ discounted_properties_list/language/en-GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JOMRES_SHORTCODES_06000DISCOUNTED_PROPERTIES_LIST',"List properties that have activated either the Wiseprice Property Config option, or the Lastminute Property Config option and thereby offers discounts.");
---
+++ discounted_properties_list/language/fr-FR.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JOMRES_SHORTCODES_06000DISCOUNTED_PROPERTIES_LIST',"Répertorie les établissements qui ont activés l'option Wiseprice Property Config (configuration judicieuse des prix des établissements) ou Lastminute Property Config (configuration des prix de dernière minutes de l'établissement) et offrent ainsi des remises.");
