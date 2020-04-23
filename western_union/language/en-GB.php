2681a451eec493647f4b7045a288956584a01620
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 18:35:37 -0700

    Add files via upload

---
+++ western_union/language/en-GB.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_WESTERN_UNION',"Western Union");
+jr_define('_JRPORTAL_WESTERN_UNION_TITLE',"Western Union Settings");
+jr_define('_JRPORTAL_WESTERN_UNION_ADMIN_INSTRUCTIONS',"Here you can define the WU account details that will be used for all properties that have the WU Gateway enabled. If you choose to override the WU account details set in frontend, then the details set here will be used. If you chosee not to, then the WU account details defined in frontend gateway config will be used.");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_OVERRIDE_ACTIVE',"Override WU account details set for all properties? ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER',"Western Union Branch: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN',"Beneficiary Name: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT',"Beneficiary ID: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC',"Beneficiary Address: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME',"Other Info: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER1',"Western Union Branch: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN1',"Beneficiary Name: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT1',"Beneficiary ID: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC1',"Beneficiary Address: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME1',"Other Info: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER2',"Western Union Branch: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN2',"Beneficiary Name: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT2',"Beneficiary ID: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC2',"Beneficiary Address: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME2',"Other Info: ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_TITLE',"Payment details for your booking at ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_HELLO',"Dear ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_THANKYOU',"Thank you for your booking at ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_SENDDEPOSITTO',"In order to secure your booking, please send your deposit of ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_BELOWADDRESS_EMAIL'," to the Western Union account below.");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_BELOWADDRESS'," to the Western Union account below. A copy of the Western Union account information will also be sent to you by email after you submit this booking.");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS1',"If you have any questions, please do not hesitate to contact us. You can ring us on ");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS2'," or email us at ");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS_EMAIL',"If you have any questions, please do not hesitate to contact us.");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_PRINT',"Print Western Union account details");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_SUBMIT',"Submit");
---
+++ western_union/language/fr-FR.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_WESTERN_UNION',"Western Union");
+jr_define('_JRPORTAL_WESTERN_UNION_TITLE',"Réglages de Western Union");
+jr_define('_JRPORTAL_WESTERN_UNION_ADMIN_INSTRUCTIONS',"Ici, vous pouvez définir les détails du compte WU qui seront utilisés pour toutes les établissements pour lesquelles la passerelle WU est activée. Si vous choisissez de remplacer les détails du compte WU définis dans l'interface, les détails définis ici seront utilisés. Si vous avez choisi de ne pas le faire, les détails du compte WU définis dans la configuration de la passerelle frontend seront utilisés.");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_OVERRIDE_ACTIVE',"Remplacer les détails du compte WU pour toutes les établissements ? ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER',"Succursale 1 de Western Union : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN',"IBAN du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT',"SWIFT du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC',"BIC du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME',"Autres informations : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER1',"Succursale 2 de Western Union : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN1',"IBAN du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT1',"SWIFT du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC1',"BIC du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME1',"Autres informations : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_ACCOUNT_HOLDER2',"Succursale 3 de Western Unionh: ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_IBAN2',"IBAN du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_SWIFT2',"SWIFT du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_BIC2',"BIC du bénéficiaire : ");
+jr_define('_JOMRES_WESTERN_UNION_GATEWAY_BANK_NAME2',"Autres informations : ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_TITLE',"Détails du paiement pour votre réservation à ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_HELLO',"Cher(e) ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_EMAIL_THANKYOU',"Merci pour votre réservation à ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_SENDDEPOSITTO',"Afin de sécuriser votre réservation, s'il vous plaît envoyez votre acompte de  ");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_BELOWADDRESS_EMAIL'," sur le compte Western Union ci-dessous.");
+jr_define('_JOMRES_CUSTOMTEXT_WESTERN_UNION_BELOWADDRESS'," sur le compte Western Union ci-dessous. Une copie des informations de compte Western Union vous sera également envoyée par mail après l'envoi de cette réservation.");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS1',"Si vous avez des questions, n'hésitez pas à nous contacter. Vous pouvez nous appeler au  ");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS2'," ou par mail à ");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_CONTACTUS_EMAIL',"Si vous avez des questions, n'hésitez pas à nous contacter.");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_PRINT',"Imprimer les détails du compte Western Union");
+jr_define('_JOMRES_WESTERN_UNION_CUSTOMTEXT_SUBMIT',"Envoyer");
