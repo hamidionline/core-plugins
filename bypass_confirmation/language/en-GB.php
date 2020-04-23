85001d3e0c6bb6604e916237d03fb9add339235b
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 15:00:01 -0700

    Add files via upload

---
+++ bypass_confirmation/language/en-GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_BYPASS_CONFIRMATION_TITLE',"Bypass review booking page");
+jr_define('_BYPASS_CONFIRMATION_DESC',"When this is enabled, the review booking page won`t be shown anymore and insetad guests will be taken directly to payment, or if no gateways are enabled, the booking will be inserted directly.");
---
+++ bypass_confirmation/language/fr-FR.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 21/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_BYPASS_CONFIRMATION_TITLE',"Gestion de la page des avis de réservation (Bypass)");
+jr_define('_BYPASS_CONFIRMATION_DESC',"Lorsque cette option est activée, la page des avis de réservation ne sera plus affichée et les clients seront directement dirigés vers le paiement. Si aucune passerelle de paiement n’est activée, la réservation sera insérée directement.");
