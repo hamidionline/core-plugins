447438b2f33c16412d0ff5b134a938aff886fa9d
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 14:51:59 -0700

    Add files via upload

---
+++ cleaning_schedule/language/en-GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_CLEANING_SCHEDULE',"Cleaning Schedule");
+
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE',"Arrival/Cleaning schedule");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_PROPERTYNAME',"Property name");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_ROOMNAME',"Room name/number");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_ARRIVAL',"Arrival");
---
+++ cleaning_schedule/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_CLEANING_SCHEDULE',"Programme de nettoyage");
+
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE',"Nettoyage des arrivés");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_PROPERTYNAME',"Nom de l'établissement");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_ROOMNAME',"Nom et Numéro de la chambre");
+jr_define('_JOMRES_CUSTOMTEXT_CLEANINGSCHEDULE_ARRIVAL',"Arrivé");
