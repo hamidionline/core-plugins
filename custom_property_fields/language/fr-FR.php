97ed2bbe845faa0aca8cf439dec85c99c35a5a02
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 16:39:22 -0700

    Add files via upload

---
+++ custom_property_fields/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_CUSTOM_PROPERTY_FIELDS_TITLE',"Champs personnalisés d'établissements");
+jr_define('_JOMRES_CUSTOM_PROPERTY_FIELDS_TITLE_EDIT','Modifier les champs personnalisés d\'établissements');
+jr_define('_JOMRES_CUSTOM_PROPERTY_FIELDS_INFO',"Utilisez cette fonctionnalité pour créer des champs d'informations personnalisés pour les établissements. Ces informations sont ajoutées par un gestionnaire d'établissements et affichées dans un nouvel onglet de la page des établissements.");
+jr_define('_JOMRES_CUSTOM_PROPERTY_FIELDS_MANAGER_TITLE',"Autre information des l'établissements");
+jr_define('_JOMRES_CUSTOM_PROPERTY_FIELDS_INSTRUCTIONS',"Ces champs personnalisés d'établissements peuvent être affichés dans les détails de l'établissement dans un onglet séparé ou dans la liste des établissements. Vous devez modifier manuellement le fichier HTML situé à /core-plugins/custom_property_fields/templates/tabcontent_01_custom_property_fields.html (pour l'onglet détaillé de l'établissement) ou/et le fichier HTML situé à /core-plugins/custom_property_fields/templates/propertylist_custom_property_fields.html (pour les extraits/snippets de la liste de l'établissement), afin d' obtenir la mise en page souhaitée. Avec les champs entrés ci-dessus, une présentation de base ressemblerait à ce qui suit, que vous pouvez l'utiliser comme exemple pour créer votre propre présentation.");
