079a0e5a0a9296e799edb49845872dc4213be8f3
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 16:33:03 -0700

    Add files via upload

---
+++ custom_global_strings/language/en-GB.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS','Global strings');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_INFO','This page shows any custom global strings that you may have created. On occasion it has been known for people to use the editing mode whilst the Global Editing mode was enabled, and inadvertantly create global property information. This may not be desired, and this page will give you the ability to remove any custom global strings that you do not need. Click the waste basket icon to remove unwanted strings.');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_ID','Database id');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_CONSTANT','Constant');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_CUSTOMTEXT','Customised text');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_LANG','Language');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_DELETE','Delete');
---
+++ custom_global_strings/language/fr-FR.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 14/08/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS','Chaînes globales de caractères');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_INFO','Cette page affiche toutes les chaînes globales de caractères personnalisées que vous avez éventuellement créées. Il est connu que certaines personnes utiliseraient le mode édition, alors que le mode édition globale (Global Editing) était activé et créraient par inadvertance des informations globales d\'établissement . Cela n\'est pas souhaitable, et cette page vous donnera la possibilité de supprimer toutes les chaînes globales de caractères personnalisées dont vous n’avez pas besoin. Cliquez sur l\'icône de la corbeille pour supprimer les chaînes indésirables.');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_ID','ID de la base de données');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_CONSTANT','Constante');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_CUSTOMTEXT','Texte personnalisé');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_LANG','Langage');
+/**
+* @ignore
+*/
+jr_define('_JOMRES_GLOBALSTRINGS_DELETE','Supprimer');
