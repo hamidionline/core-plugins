c38460f48381eb360189472d6c9b452c2cb1fdc5
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 01:15:18 -0700

    Add files via upload

---
+++ property_list_swipe/language/fr-FR.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 30/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_PROPERTYLIST_SWIPE',"Swipe (caroussel d'établissement)");
+
