23c47c85e2ee9368eb5effb8e581904264113826
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 00:23:26 -0700

    Add files via upload

---
+++ default_property_settings/language/en-GB.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_TITLE',"Default property settings");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_CONFIGTITLE',"General Config title (if available, some may not be)");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_VARIABLENAME',"Variable name");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_CURRENTVALUE',"Current value");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_WARNING',"This is an advanced feature, if you don't have a specific use for it, it's better that you don't use it.<br/> This plugin allows you to modify the default values of a new property. Make sure you understand what the variable does before changing it as changing it here may have far reaching affects on your Jomres system.</br>Blank items are generally older settings that are not used any more, but left in situ for backward compatability reasons, or are settings that can't be modified via the Property Configuration area.");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_NEWVALUE',"New value");
---
+++ default_property_settings/language/fr-FR.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_TITLE',"Paramètres par défaut d'établissement ");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_CONFIGTITLE',"Titre de la configuration générale (si disponible, certains peuvent ne pas l'être)");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_VARIABLENAME',"Nom de la variable");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_CURRENTVALUE',"Valeur actuelle");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_WARNING',"Il s’agit d’une fonctionnalité avancée. Si vous n’en avez pas l’utilisation, il est préférable de ne pas s'en servir. <br/> Ce plug-in vous permet de modifier les valeurs par défaut d’un nouvel établissement. Assurez-vous de bien comprendre le rôle de la variable avant de la modifier, car cela pourrait avoir des conséquences considérables sur Jomres. </ Br> Les éléments vides sont généralement des paramètres plus anciens qui ne sont plus utilisés, mais qui sont laissés in situ pour des raisons de compatibilité avec les versions antérieures, ou sont des paramètres qui ne peuvent pas être modifiés via la Configuration de l'établissement.");
+jr_define('_JRPORTAL_DEFAULT_PROPERTY_SETTINGS_NEWVALUE',"Nouvelle valleur");
