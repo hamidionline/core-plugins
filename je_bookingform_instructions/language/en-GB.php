ed947d9714260744ee4bbc8cb7b7cefc92f53a0d
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:51:31 -0700

    Add files via upload

---
+++ je_bookingform_instructions/language/en-GB.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_TITLE',"Booking form instructions");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_YESNO',"Enabled? ");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_YES',"Yes");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_NO',"No");
---
+++ je_bookingform_instructions/language/fr-FR.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_TITLE',"Instructions du formulaire de réservation");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_YESNO',"Activées ? ");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_YES',"Oui");
+jr_define('_JE_BOOKINGFORM_INSTRUCTIONS_ENABLED_NO',"Non");
