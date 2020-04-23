3b2c8f227e3b17fffb3e5f7950dd4606379749f3
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 16:53:39 -0700

    Add files via upload

---
+++ embed_availability_calendar/language/en-GB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JINTOUR_EMBED_CALENDAR_TITLE',"Embed Availability Calendar");
+jr_define('_JINTOUR_EMBED_CALENDAR_INSTRUCTIONS',"Here is the code you will need to embed the availability calendar into the web page of another site.");
+jr_define('_JINTOUR_EMBED_CODE',"Code");
---
+++ embed_availability_calendar/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JINTOUR_EMBED_CALENDAR_TITLE',"Calendrier intégré de disponibilités");
+jr_define('_JINTOUR_EMBED_CALENDAR_INSTRUCTIONS',"Voici le code dont vous aurez besoin pour intégrer le calendrier de disponibilité dans la page Web d'un autre site.");
+jr_define('_JINTOUR_EMBED_CODE',"Code");
