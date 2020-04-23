c429145bfdc43351d75006ffc8c561a563efc601
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 20:10:43 -0700

    Add files via upload

---
+++ asamodule_latest/language/en-GB.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST',"Asamodule Latest. Shows the most recently added properties.");
+
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_LISTLIMIT',"The maximum number of properties that can be shown.");
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_PTYPE_IDS',"Show only properties of this specific property type.");
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_VERTICAL',"Set to 1 to show the modules optimised for a vertical widget/module position.");
---
+++ asamodule_latest/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 19/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST',"Asamodule Latest. Affiche les dernières établissements ajoutées.");
+
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_LISTLIMIT',"Le nombre maximum d’établissements pouvant être affichés.");
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_PTYPE_IDS',"Afficher uniquement les établissements de ce type.");
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_LATEST_ARG_ASAMODULE_POPULAR_VERTICAL',"Définissez la valeur sur 1 pour afficher les modules optimisés en position verticale.");
