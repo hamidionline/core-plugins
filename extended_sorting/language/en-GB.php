192e39bd440d241f19a23a319b4fd7a1cd96559a
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:09:08 -0700

    Add files via upload

---
+++ extended_sorting/language/en-GB.php
@@ -0,0 +1,32 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SORTING_PLUGIN_DEFAULT',"Default (no ordering)");
+jr_define('_JOMRES_SORTING_PLUGIN_PROPERTYNAME',"Property Name (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_PROPERTYNAME_DESC',"Property Name (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_REGION',"Region (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_REGION_DESC',"Region (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_TOWN',"Town (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_TOWN_DESC',"Town (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_STARS',"Stars (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_STARS_DESC',"Stars (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_MOST_POPULAR_CLICKS',"Most viewed (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_MOST_POPULAR_CLICKS_DESC',"Most viewed (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_GUESTS',"Guests (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_GUESTS_DESC',"Guests (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_PRICE',"Price (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_PRICE_DESC',"Price (descending)");
+jr_define('_JOMRES_SORTING_PLUGIN_RANDOMIZE',"Randomize");
+jr_define('_JOMRES_SORTING_PLUGIN_LISTING_AGE',"Date added (ascending)");
+jr_define('_JOMRES_SORTING_PLUGIN_LISTING_AGE_DESC',"Date added (descending)");
---
+++ extended_sorting/language/fr-FR.php
@@ -0,0 +1,33 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SORTING_PLUGIN_DEFAULT',"Par défaut (pas de commande)");
+jr_define('_JOMRES_SORTING_PLUGIN_PROPERTYNAME',"Noms des établissements (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_PROPERTYNAME_DESC',"Noms des établissements (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_REGION',"Régions (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_REGION_DESC',"Régions (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_TOWN',"Villes (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_TOWN_DESC',"Villes (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_STARS',"Étoiles (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_STARS_DESC',"Étoiles (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_MOST_POPULAR_CLICKS',"le plus vu (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_MOST_POPULAR_CLICKS_DESC',"Le plus vu (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_GUESTS',"Clients (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_GUESTS_DESC',"Clients (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_PRICE',"Prix (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_PRICE_DESC',"Prix (décroissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_RANDOMIZE',"Aléatoirement");
+jr_define('_JOMRES_SORTING_PLUGIN_LISTING_AGE',"Dates ajoutées (croissant)");
+jr_define('_JOMRES_SORTING_PLUGIN_LISTING_AGE_DESC',"Dates ajoutées (décroissant)");
