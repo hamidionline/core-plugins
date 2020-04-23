0747db370329d918c2264a82809b527852869423
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 05:05:01 -0700

    Add files via upload

---
+++ income_table/language/en-GB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_INCOME_TABLE_TITLE',"Income table");
+jr_define('_JOMRES_INCOME_TABLE_MONTH',"Month");
+jr_define('_JOMRES_INCOME_TABLE_INCOME',"Income (Booked out properties only) ");
---
+++ income_table/language/fr-FR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 26/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_INCOME_TABLE_TITLE',"Table des revenus");
+jr_define('_JOMRES_INCOME_TABLE_MONTH',"Mois");
+jr_define('_JOMRES_INCOME_TABLE_INCOME',"Revenus (établissements réservés uniquement) ");
