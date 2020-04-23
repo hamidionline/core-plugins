d61c17c2e6bb87ec1446e3f3e75c24b8ebf6e776
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 23:09:51 -0700

    Add files via upload

---
+++ je_fps/language/en-GB.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define('_JE_FPS_MODULE_TITLE',"Featured Listings");
---
+++ je_fps/language/fr-FR.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define('_JE_FPS_MODULE_TITLE',"Listes des équipements");
