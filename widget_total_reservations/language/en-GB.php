611d67b57debcdfec3aa5d2ab84ba1e2ba44ac19
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 06:12:37 -0700

    Add files via upload

---
+++ widget_total_reservations/language/en-GB.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 9.x
+ * @package Jomres
+ * @copyright	2005-2017 Vince Wooll
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+ 
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('WIDGET_TOTAL_RESERVATIONS',"Total Reservations");
+jr_define('WIDGET_TOTAL_RESERVATIONS_PENDING',"Pending");
+jr_define('WIDGET_TOTAL_RESERVATIONS_COMPLETED',"Completed");
+jr_define('WIDGET_TOTAL_RESERVATIONS_CANCELLED',"Cancelled");
---
+++ widget_total_reservations/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Core file
+ *
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 9.x
+ * @package Jomres
+ * @copyright	2005-2017 Vince Wooll
+ * Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+ * Jomres (tm) PHP, CSS & Javascript files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly.
+ **/
+ 
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('WIDGET_TOTAL_RESERVATIONS',"Total des réservations");
+jr_define('WIDGET_TOTAL_RESERVATIONS_PENDING',"En attente");
+jr_define('WIDGET_TOTAL_RESERVATIONS_COMPLETED',"Confirmées");
+jr_define('WIDGET_TOTAL_RESERVATIONS_CANCELLED',"Annulées");
