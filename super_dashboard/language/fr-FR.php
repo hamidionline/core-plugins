9b7449bb83c796424b10adf8e6b9a95568638009
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 18:50:12 -0700

    Add files via upload

---
+++ super_dashboard/language/fr-FR.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 30/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('LEGACY_DASHBOARD',"Tableau de bord");
