d75f4c16e7c626d90cab6936e99bdd5b81ea37ad
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:44:21 -0700

    Add files via upload

---
+++ je_alternative_properties/language/fr-FR.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2013 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_ALTERNATIVE_PROPERTIES_TITLE',"Établissements alternatifs");
+jr_define('_JRPORTAL_ALTERNATIVE_PROPERTIES_TITLE_FRONTEND',"Vous pouvez également être intéressé par...");
+jr_define('_JRPORTAL_ALTERNATIVE_PROPERTIES_HLISTLIMIT',"Nombre maximum de suggestions d'établissements à afficher ");
