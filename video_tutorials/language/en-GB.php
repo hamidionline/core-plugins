733c1405cde665e85345c52c6f1579233acd21d0
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 13:32:04 -0700

    Add files via upload

---
+++ video_tutorials/language/en-GB.php
@@ -0,0 +1,9 @@
+<?php
+
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_GUEST_TYPES', 'Translating guest types');
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_GUEST_TYPES_DESC', 'This video will show you how to translate/modify a label (guest type in this case) to a different language. Editing mode is property specific, so for one property you can translate it to one thing, and for another property to something else. This also works for the same language so it is an easy way to give different properties different labels for the same area of the page.');
+
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_PROPERTY_DETAILS', 'Translating property content');
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_PROPERTY_DETAILS_DESC', "Here you will see how simple it is to translate the content of your property details page. Save your content in language A. Swtich to language B and save it again. That's it!");
---
+++ video_tutorials/language/fr-FR.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_GUEST_TYPES', 'Traduction des types de clients');
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_GUEST_TYPES_DESC', 'Cette vidéo vous montrera comment traduire/modifier une étiquette (type de client dans ce cas) dans une autre langue. Le mode d\'édition étant spécifique à un établissement, vous pouvez la traduire pour un établissement et pour un autre. Cela fonctionne également pour la même langue, ce qui permet de donner facilement à différents établissement différentes étiquettes pour la même zone de la page.');
+
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_PROPERTY_DETAILS', 'Traduction du contenu d\'un établissement');
+jr_define('_JOMRES_TUTORIAL_TRANSLATE_PROPERTY_DETAILS_DESC', "Ici, vous verrez à quel point il est simple de traduire le contenu de la page de détails de votre établissement. Enregistrez votre contenu dans la langue A. Basculez vers la langue B et enregistrez-le à nouveau. C'est tout !");
