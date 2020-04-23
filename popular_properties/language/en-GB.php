00d1962eece78012d8073e99f92fb5394eb5db74
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 00:38:15 -0700

    Add files via upload

---
+++ popular_properties/language/en-GB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_DESTINATIONS_REVIEWS','Reviews');
+jr_define('_JOMRES_DESTINATIONS_SCORE','Score');
+jr_define('_JOMRES_DESTINATIONS_ALLPROPERTIES','List all rated properties');
+jr_define('_JOMRES_DESTINATIONS_MENUNAME','Popular Properties');
---
+++ popular_properties/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 29/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_DESTINATIONS_REVIEWS','Avis');
+jr_define('_JOMRES_DESTINATIONS_SCORE','Notation');
+jr_define('_JOMRES_DESTINATIONS_ALLPROPERTIES','Lister toutes les établissements notés');
+jr_define('_JOMRES_DESTINATIONS_MENUNAME','Établissements bien notés');
