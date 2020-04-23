477e5183b70ecf941f814ca18a64bb901c1e7c56
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 21:24:07 -0700

    Add files via upload

---
+++ featured_listings_slider_1/language/en-GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_SLIDER_1', "Featured Listings Slider 1. " );
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_SLIDER_1_LIMIT', "Optional. Default 5 if not set. The number of properties to show." );
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_SLIDER_1_PTYPE_IDS', "Optional. A comma seperated list of property type ids." );
+
---
+++ featured_listings_slider_1/language/fr-FR.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_SLIDER_1', "Diaporama des listes des équipements 1. " );
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_SLIDER_1_LIMIT', "Optionnel. Par défaut 5, si non défini. Le nombre d'établissement à afficher." );
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_SLIDER_1_PTYPE_IDS', "Optionnel. Liste d'es IDs de types d'établissements, séparés par des virgules." );
+
