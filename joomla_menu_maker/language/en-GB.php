ef38b16af164c924f1e5c41b98b12ed9aecc195f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:19:24 -0700

    Add files via upload

---
+++ joomla_menu_maker/language/en-GB.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+define('_JOMRES_JOOMLA_MENUMAKER_TITLE',"Menu Maker");
+define('_JOMRES_JOOMLA_MENUMAKER_MENUOPTIONS',"Add this page to a Joomla menu");
+define('_JOMRES_JOOMLA_MENUMAKER_INFO',"To add this page to a Joomla Menu please give it a title and click the Save button. The page will be added to the selected Joomla menu.");
+define('_JOMRES_JOOMLA_MENUMAKER_SAVE',"Save page to menu");
+define('_JOMRES_JOOMLA_MENUMAKER_SEARCHTITLE',"Menu title");
+define('_JOMRES_JOOMLA_MENUMAKER_JOOMLA_MENU',"Joomla menu");
+
---
+++ joomla_menu_maker/language/fr-FR.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+define('_JOMRES_JOOMLA_MENUMAKER_TITLE',"Créateur de menu");
+define('_JOMRES_JOOMLA_MENUMAKER_MENUOPTIONS',"Ajouter cette page à un menu Joomla");
+define('_JOMRES_JOOMLA_MENUMAKER_INFO',"Pour ajouter cette page à un menu Joomla, donnez-lui un titre et cliquez sur le bouton Enregistrer. La page sera ajoutée au menu Joomla sélectionné.");
+define('_JOMRES_JOOMLA_MENUMAKER_SAVE',"Enregistrer la page dans le menu");
+define('_JOMRES_JOOMLA_MENUMAKER_SEARCHTITLE',"Titre du menu");
+define('_JOMRES_JOOMLA_MENUMAKER_JOOMLA_MENU',"Menu Joomla");
+
