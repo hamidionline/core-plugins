6e6875f8b6c5681323c295bc59aa7cd6a058f29c
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 20:58:22 -0700

    Add files via upload

---
+++ featured_listings_asamodule_1/language/en-GB.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_ASAMODULE_TITLE_A',"Featured Listings Layout 1");
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1', "Featured Listings Layout 1. Note, will not be displayed on a page where search results are being. " );
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1_FEATURED_LISTINGS_ASAMODULE_1_limit', "Option. Default 5 if not set. The number of properties to show." );
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1_FEATURED_LISTINGS_ASAMODULE_1_ptype_ids', "Optional. A comma seperated list of property type ids." );
+
+
+
---
+++ featured_listings_asamodule_1/language/fr-FR.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_ASAMODULE_TITLE_A',"Mise en page de la liste des équipements 1");
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1', "Mise en page de la liste des équipements 1. Remarque : ne sera pas affiché sur une page où les résultats de la recherche sont en cours. " );
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1_FEATURED_LISTINGS_ASAMODULE_1_limit', "Option. Par défaut 5, si non défini. Le nombre d'établissements à afficher." );
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_1_FEATURED_LISTINGS_ASAMODULE_1_ptype_ids', "Optionnel. Liste des IDs des types d'établissements, séparés par des virgules." );
+
+
+
