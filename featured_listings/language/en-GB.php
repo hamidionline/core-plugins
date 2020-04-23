f03303dddf420fef73dd98d87effdedcf2e4ead9
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 20:46:47 -0700

    Add files via upload

---
+++ featured_listings/language/en-GB.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_TITLE',"Featured Listings");
+jr_define('_JRPORTAL_FEATUREDLISTINGS',"Feature");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_ORDER',"Order");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_EMPHASIS',"Featured listings can have a CSS class applied to them when shown in the property list. Define here a CSS class or classes that will wrap the property area.");
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_CONFIG_PRIORITISE',"Prioritise featured listings?");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_CONFIG_PRIORITISE_DESC',"By default featured properties are listed first in search results. Set this option to No to disable this behaviour.");
---
+++ featured_listings/language/fr-FR.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_TITLE',"Liste des équipements");
+jr_define('_JRPORTAL_FEATUREDLISTINGS',"Équipements");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_ORDER',"Ordre");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_EMPHASIS',"Une class CSS peut être appliquée aux listes des équipement dans la liste des établissements. Définissez ici une ou plusieurs class CSS qui engloberont la mise en page des établissements.");
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_CONFIG_PRIORITISE',"Prioriser les listes des équipements ?");
+jr_define('_JRPORTAL_FEATUREDLISTINGS_CONFIG_PRIORITISE_DESC',"Par défaut, les équipements des établissements sont répertoriés en premier dans les résultats de recherche. Définissez cette option sur Non pour désactiver ce comportement.");
