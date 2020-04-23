9f93b3017ac072e8259d1a249f233907fdb1b986
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 18:43:31 -0700

    Add files via upload

---
+++ wire_transfer/language/fr-FR.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_WIRE_TRANSFER',"Virement bancaire");
+jr_define('_JRPORTAL_WIRE_TRANSFER_TITLE',"Régalges du virement bancaire");
+jr_define('_JRPORTAL_WIRE_TRANSFER_ADMIN_INSTRUCTIONS',"Ici, vous pouvez définir les détails du compte bancaire qui seront utilisés pour toutes les établissements pour lesquels le paiement par virement bancaire est activé. Si vous choisissez de remplacer les détails du compte bancaire définis dans l'interface, les détails définis ici seront utilisés. Si vous avez choisi de ne pas le faire, les détails du compte bancaire définis dans la configuration sur le site de la passerelle seront utilisés.");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_OVERRIDE_ACTIVE',"Remplacer les détails du compte bancaire définis pour toutes les établissements ?");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_ACCOUNT_HOLDER',"Titulaire de compte 1 : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_IBAN',"IBAN : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_SWIFT',"SWIFT : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_BIC',"BIC : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_NAME',"Nom de la banque : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_ACCOUNT_HOLDER1',"Titulaire de compte 2 : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_IBAN1',"IBAN: ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_SWIFT1',"SWIFT : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_BIC1',"BIC : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_NAME1',"Nom de la banque : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_ACCOUNT_HOLDER2',"Titulaire de compte 3 : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_IBAN2',"IBAN: ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_SWIFT2',"SWIFT : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_BIC2',"BIC : ");
+jr_define('_JOMRES_WIRE_TRANSFER_GATEWAY_BANK_NAME2',"Nom de la banque : ");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_EMAIL_TITLE',"Détails de paiement pour votre réservation à ");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_EMAIL_HELLO',"Cher(e) ");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_EMAIL_THANKYOU',"Merci pour votre réservation à ");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_SENDDEPOSITTO',"Afin de sécuriser votre réservation, veuillez transférer votre acompte de ");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_BELOWADDRESS_EMAIL'," sur le compte bancaire ci-dessous.");
+jr_define('_JOMRES_CUSTOMTEXT_WIRE_TRANSFER_BELOWADDRESS'," sur le compte bancaire ci-dessous. Une copie des informations du compte bancaire vous sera également envoyée par mail après la validation de votre réservation.");
+jr_define('_JOMRES_WIRE_TRANSFER_CUSTOMTEXT_CONTACTUS1',"Si vous avez des questions, n'hésitez pas à nous contacter. Vous pouvez nous appeler au ");
+jr_define('_JOMRES_WIRE_TRANSFER_CUSTOMTEXT_CONTACTUS2'," ou par mail à ");
+jr_define('_JOMRES_WIRE_TRANSFER_CUSTOMTEXT_CONTACTUS_EMAIL',"Si vous avez des questions, n'hésitez pas à nous contacter.");
+jr_define('_JOMRES_WIRE_TRANSFER_CUSTOMTEXT_PRINT',"Imprimer les détails du compte bancaire");
+jr_define('_JOMRES_WIRE_TRANSFER_CUSTOMTEXT_SUBMIT',"Envoyer");
