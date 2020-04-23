4e79c16b6f3a72a1f85e9a9dad91541defc07d20
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 00:57:43 -0700

    Add files via upload

---
+++ property_inna_module/language/en-GB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_SHORTCODES_06000PROPERTY_INNA_MODULE', 'Show a specific property in a small box' );
+jr_define( '_JOMRES_SHORTCODES_06000PROPERTY_INNA_MODULE_ARG_PROPERTY_UID', 'The property uid.' );
+
