8874aa03f6b0b1fb66b684dec76978debb8a3bbf
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:58:50 -0700

    Add files via upload

---
+++ je_custom_html_tab/language/en-GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_TITLE',"Custom HTML Content");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_TITLE_TAB',"Custom HTML");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_YESNO',"Enabled? ");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_YES',"Yes");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_NO',"No");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_INSTRUCTIONS_FRONTEND',"Content ");
---
+++ je_custom_html_tab/language/fr-FR.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2012 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_TITLE',"Contenu HTML personnalisé");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_TITLE_TAB',"Personnaliser l'HTML");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_YESNO',"Activé ? ");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_YES',"Oui");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_ENABLED_NO',"Non");
+jr_define('_JRPORTAL_JE_CUSTOM_HTML_TAB_INSTRUCTIONS_FRONTEND',"Contenu ");
