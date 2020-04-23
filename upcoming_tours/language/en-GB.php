fc2a6cbc77eedb70d3addb329d209f7cba3949f7
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 13:39:44 -0700

    Add files via upload

---
+++ upcoming_tours/language/en-GB.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SHORTCODES_06000UPCOMING_TOURS',"Upcoming tours");
+
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_POPULAR_ARG_UPCOMING_TOURS_LISTLIMIT',"The maximum number of properties to show.");
+jr_define('_JOMRES_SHORTCODES_06000ASAMODULE_POPULAR_ARG_UPCOMING_TOURS_VERTICAL',"Align vertically?");
+
+
+
+
+
+
+
