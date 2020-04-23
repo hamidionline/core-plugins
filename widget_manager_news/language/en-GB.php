c08f56249d71a12013f6df795572ecf03f54c178
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 13:26:47 -0700

    Add files via upload

---
+++ widget_manager_news/language/en-GB.php
@@ -0,0 +1,19 @@
+<?php
+
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+
+jr_define('WIDGET_MANAGER_NEWS_TITLE',"Site News");
+jr_define('WIDGET_MANAGER_NEWS_DESCRIPTION',"From here you can create small snippets of site/news articles that can be viewed in the Property Manager's control panel. You can give it a title, a small snippet of text and an optional url. You can also change the alert style to give different messages different colours : success/info/warning/danger");
+
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_TITLE',"Article title");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_CONTENT',"Article content");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_URL',"URL (optional)");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_CONTEXT',"Style (success/info/warning/danger)");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_DATE',"Date");
+
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_SUCCESS',"Success");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_INFO',"Info");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_WARNING',"Warning");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_DANGER',"Danger");
+
+jr_define('WIDGET_MANAGER_NEWS_TARGET_PROPERTY',"Target Property");
---
+++ widget_manager_news/language/fr-FR.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('WIDGET_MANAGER_NEWS_TITLE','Actualités du site');
+jr_define('WIDGET_MANAGER_NEWS_DESCRIPTION','Vous pouvez créer de petits extraits d\'articles de site/d\'actualités pouvant être consultés dans le panneau de configuration du gestionnaire des établissements. Vous pouvez lui donner un titre, un petit extrait de texte et une URL optionnelle. Vous pouvez également modifier le style d\'alerte pour donner à différents messages différentes couleurs : succès/info/avertissement/danger');
+
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_TITLE',"Titre de l'article");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_CONTENT',"Contenu de l'article");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_URL',"URL (optionel)");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_CONTEXT',"Style (succès/info/avertissement/danger)");
+jr_define('WIDGET_MANAGER_NEWS_ARTICLE_DATE',"Date");
+
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_SUCCESS',"Succès");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_INFO',"Info");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_WARNING',"Avertissement");
+jr_define('WIDGET_MANAGER_NEWS_CONTEXT_DANGER',"Danger");
+
+jr_define('WIDGET_MANAGER_NEWS_TARGET_PROPERTY',"Établissement cible");
