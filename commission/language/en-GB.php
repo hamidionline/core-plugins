e1fc01ec60573236db21b1a7ad24b9a3924a7ad5
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 17:43:52 -0700

    Add files via upload

---
+++ commission/language/en-GB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_COMMISSION',"Commission");
+jr_define('_JOMRES_COMMISSION_ASSIGN',"Assign Commission Rates");
+jr_define('_JOMRES_CHART_COMMISSION_DESC',"Income by year/month");
---
+++ commission/language/fr-FR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 21/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_COMMISSION',"Commission");
+jr_define('_JOMRES_COMMISSION_ASSIGN',"Attribuer des taux de commission");
+jr_define('_JOMRES_CHART_COMMISSION_DESC',"Revenu par année/mois");
