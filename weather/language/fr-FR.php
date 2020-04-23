9ea93d481bda20a9bf3dca742874d0bb3c4647d7
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 18:27:10 -0700

    Add files via upload

---
+++ weather/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_CURRENT_WEATHER', 'Météo' );
+jr_define( '_OPENWEATHER_WEATHER', 'Intégration de Open Weather' );
+jr_define( '_OPENWEATHER_WEATHER_APIKEY', 'Clef API Open Weather' );
+jr_define( '_OPENWEATHER_WEATHER_APIKEY_DESC', 'Vous pouvez obtenir une clé API Open Weather gratuite à l\'adresse http://openweathermap.org/api.' );
