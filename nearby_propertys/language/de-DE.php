aed38fb717f9cffdecfc45f9210ff66911005c8b
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 22:33:12 -0700

    Add files via upload

---
+++ nearby_propertys/language/de-DE.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_TITLE',"Objekte in der N&auml;he");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_TITLE_FRONTEND',"Objekte in der N&auml;he ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HRADIUS',"Radius ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HLISTLIMIT',"Maximale Anzahl der Objekte, in der N&auml;he zu zeigen ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HPTYPE_ENABLED',"Sollen nur Objekte mit dem gleichen Objekttyp wie das aktuell angezeigte Objekt angezeigt werden? ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE'," in ungef&auml;hr ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE_KM',"km");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE_MILES',"meilen");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_INSTRUCTIONS',"Bevor Sie dieses Plugin verwenden, stellen Sie bitte sicher, dass Sie die richtigen L&auml;ngen- und Breitenkoordinaten f&uuml;r alle Objekte eingegeben haben. Es ist nicht notwendig Google Maps zu aktivieren, wenn Sie nicht m&ouml;chten. Geben Sie die Koordinaten trotzdem ein, denn diese werden f&uuml;r die Suche der Objekte in der N&auml;he im gew&auml;hlten Radius verwendet und um die Entfernung vom aktuell angezeigten Objekt zu berechnen.");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_NOTHINGNEARBY',"Keine anderen Objekte in der Nähe.");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_NOTHINGNEARBY2'," innerhalb ");
---
+++ nearby_propertys/language/fr-FR.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 29/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_TITLE',"Établissements(s) à proximité");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_TITLE_FRONTEND',"Établissements(s) à proximité ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HRADIUS',"Aux alentours de (km) ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HLISTLIMIT',"Nombre maximum d'établissement à proximté à afficher ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_HPTYPE_ENABLED',"Afficher uniquement les établissements du même type que celui sélectionné actuellement ? ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE'," aux alentours de ");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE_KM',"km");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_DISTANCE_MILES',"miles");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_INSTRUCTIONS',"Avant d'utiliser ce plug-in, vous devez vous assurer que vous avez entré les coordonnées de latitude et de longitude correctes pour toutes les établissements. Il n'est pas nécessaire d'activer Google Maps, si vous ne le souhaitez pas, mais entrez simplement les coordonnées, car elles seront utilisées pour rechercher les établissements à proximité dans le rayon choisi et calculer la distance de l’établissement actuelle sélectionné.");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_NOTHINGNEARBY',"Pas d'autre établissement à proximité.");
+jr_define('_JRPORTAL_NEARBY_PROPERTYS_NOTHINGNEARBY2'," dans ");
