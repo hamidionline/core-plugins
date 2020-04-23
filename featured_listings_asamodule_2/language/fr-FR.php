d0d8be42df84e6630c3372b5f3dd0e71eed718ce
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-22 21:05:16 -0700

    Add files via upload

---
+++ featured_listings_asamodule_2/language/fr-FR.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_FEATUREDLISTINGS_ASAMODULE_TITLE_B',"Mise en page de la liste des équipements 2");
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_ASAMODULE_2', "Mise en page de la liste des équipements 2. Remarque : ne sera pas affiché sur une page où les résultats de la recherche sont en cours. " );
+
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_2_LIMIT', "Optionnel. Par défaut 30, si non défini. Le nombre d'établissements à afficher." );
+jr_define( '_JOMRES_SHORTCODES_06000FEATURED_LISTINGS_2_PTYPE_IDS', "Optionnel. Liste des IDs de types d'établissements, séparés par des virgules." );
