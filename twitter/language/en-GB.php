4b6795c7b73762904985ee9d770c21e9d04f5fcc
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 18:54:20 -0700

    Add files via upload

---
+++ twitter/language/en-GB.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_TWITTER_TITLE',"Twitter Integration");
+jr_define('_TWITTER_HASTAGS',"Hashtags");
+jr_define('_TWITTER_HASTAGS_DESC',"A comma separated list of hashtags.");
+jr_define('_TWITTER_BOOKING_MADE_MESSAGE',"Booking made message ");
+jr_define('_TWITTER_ACCESS_TOKEN',"Access token");
+jr_define('_TWITTER_ACCESS_TOKEN_SECRET',"Access token secret");
+jr_define('_TWITTER_CONSUMER_KEY',"Consumer key");
+jr_define('_TWITTER_CONSUMER_KEY_CONSUMER_SECRET',"Consumer secret");
+jr_define('_TWITTER_MANAGER_USERNAME',"Twitter username");
+jr_define('_TWITTER_MANAGER_USERNAME_DESC',"Add your twitter username here and we'll send you a DM when a booking has been made. Before we can do that, however, please ensure that you are following this user : ");
+jr_define('_TWITTER_MANAGER_TWEET',"Hi! You've got a booking! Click the link to view it on our site ");
---
+++ twitter/language/fr-FR.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_TWITTER_TITLE',"Intégration de Twitter");
+jr_define('_TWITTER_HASTAGS',"Hashtags");
+jr_define('_TWITTER_HASTAGS_DESC',"Liste des hashtags séparés par des virgules.");
+jr_define('_TWITTER_BOOKING_MADE_MESSAGE',"La réservation a engendré un message ");
+jr_define('_TWITTER_ACCESS_TOKEN',"Jeton d'accès (Access token)");
+jr_define('_TWITTER_ACCESS_TOKEN_SECRET',"Jeton secret  d'accès (Access token secret)");
+jr_define('_TWITTER_CONSUMER_KEY',"Clé de l'utilisateur (Consumer key)");
+jr_define('_TWITTER_CONSUMER_KEY_CONSUMER_SECRET',"Clé secrète de l'utilisateur (Consumer secret)");
+jr_define('_TWITTER_MANAGER_USERNAME',"Identifiant Twitter");
+jr_define('_TWITTER_MANAGER_USERNAME_DESC',"Ajoutez votre nom d'utilisateur Twitter ici et nous vous enverrons un DM une fois la réservation effectuée. Avant de pouvoir le faire, veuillez toutefois vous assurer que vous suivez bien cet utilisateur : ");
+jr_define('_TWITTER_MANAGER_TWEET',"Bonjour et merci ! Vous avez effectué une réservation dans notre établissement ! Cliquez sur le lien pour la voir sur notre site. ");
