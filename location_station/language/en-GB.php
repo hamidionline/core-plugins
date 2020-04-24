08594d1e2b0f4030b70e746b138badac35edd5c5
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 22:38:59 -0700

    Add files via upload

---
+++ location_station/language/en-GB.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+jr_define('_JOMRES_LOCATION_STATION_TITLE','Location Station');
+jr_define('_JOMRES_LOCATION_STATION_INFORMATION','Location Information');
+
