eed972354eaabe967686102814439d35d4179a88
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 19:01:31 -0700

    Add files via upload

---
+++ show_room_calendar/language/en-GB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_SHORTCODES_06000SHOW_ROOM_CALENDAR', 'Show a calendar for a room that has been created in Jomres' );
+jr_define( '_JOMRES_SHORTCODES_06000SHOW_ROOM_CALENDAR_ARG_ROOM_UID', 'The room id that you want to show the calendar for.' );
+
---
+++ show_room_calendar/language/fr-FR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 30/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_SHORTCODES_06000SHOW_ROOM_CALENDAR', 'Afficher un calendrier pour une chambre créée dans Jomres' );
+jr_define( '_JOMRES_SHORTCODES_06000SHOW_ROOM_CALENDAR_ARG_ROOM_UID', 'L\'ID de la chambre pour laquelle vous souhaitez afficher le calendrier.' );
+
