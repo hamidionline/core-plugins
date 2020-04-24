e42965f742d5e1ececffd26454464fdf215fef90
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 21:41:27 -0700

    Add files via upload

---
+++ local_events/language/en-GB.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JRPORTAL_LOCAL_EVENTS_TITLE',"What's happening?");
+jr_define('_JRPORTAL_LOCAL_EVENTS_NOTES'," ");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EVENT_TITLE',"Event name");
+jr_define('_JRPORTAL_LOCAL_EVENTS_STARTDATE',"Start date");
+jr_define('_JRPORTAL_LOCAL_EVENTS_ENDDATE',"End date");
+jr_define('_JRPORTAL_LOCAL_EVENTS_LATITUDE',"Latitude");
+jr_define('_JRPORTAL_LOCAL_EVENTS_LONGITUDE',"Longitude");
+jr_define('_JRPORTAL_LOCAL_EVENTS_WEBSITEURL',"The site's url");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EVENTLOGORELPATH',"The relative path to the event's logo, if available. Eg. 'www.event.com/logo.png' (without 'http://') ");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_TITLE',"What to do locally");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_NOTES'," ");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ATTRACTION_TITLE',"Attraction name");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ICON',"Icon");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_LATITUDE',"Latitude");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_LONGITUDE',"Longitude");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_WEBSITEURL',"The site's url");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ATTRACTIONLOGORELPATH',"The relative path to the attraction's logo, if available. Eg. 'www.attraction.com/logo.png' (without 'http://') ");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EDIT',"Edit local event");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_EDIT',"Edit local attraction");
+
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_TITLE',"Local Events and Attractions");
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_RADIUS',"Radius limit (in Kilometers)");
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_RADIUS_DESC',"Local events and attractions will be shown underneath the property details. Define here the radius to limit the events shown to those within a specific range.");
---
+++ local_events/language/fr-FR.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 12/08/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+
+jr_define('_JRPORTAL_LOCAL_EVENTS_TITLE',"Événements");
+jr_define('_JRPORTAL_LOCAL_EVENTS_NOTES'," ");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EVENT_TITLE',"Nom de l'événement");
+jr_define('_JRPORTAL_LOCAL_EVENTS_STARTDATE',"Date de début");
+jr_define('_JRPORTAL_LOCAL_EVENTS_ENDDATE',"Date de fin");
+jr_define('_JRPORTAL_LOCAL_EVENTS_LATITUDE',"Latitude");
+jr_define('_JRPORTAL_LOCAL_EVENTS_LONGITUDE',"Longitude");
+jr_define('_JRPORTAL_LOCAL_EVENTS_WEBSITEURL',"URL du site");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EVENTLOGORELPATH',"Le chemin relatif au logo de l'événement, si disponible. Par exemple : www.evenement.com/logo.png (sans https://) ");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_TITLE',"Activités");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_NOTES'," ");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ATTRACTION_TITLE',"Nom de l'activité");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ICON',"Icône");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_LATITUDE',"Latitude");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_LONGITUDE',"Longitude");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_WEBSITEURL',"URL du site");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_ATTRACTIONLOGORELPATH',"Le chemin relatif au logo de l'activité, si disponible. Par exemple : www.activite.com/logo.png (sans http://) ");
+jr_define('_JRPORTAL_LOCAL_EVENTS_EDIT',"Modifier l'événement local");
+jr_define('_JRPORTAL_LOCAL_ATTRACTIONS_EDIT',"Modifier l'activité locale");
+
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_TITLE',"Activités et événements locaux");
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_RADIUS',"Aux alentours en kilomètres)");
+jr_define('_JRPORTAL_LOCAL_EVENTS_SETTINGS_RADIUS_DESC',"Les événements et les activités locaux seront affichés sous les détails des établissements. Définissez ici le rayon de distance pour limiter les événements affichés (en km).");
