154f7a550ca077b5fcd4c776b7e582c49819045e
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 22:56:04 -0700

    Add files via upload

---
+++ magic_menu/language/en-GB.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_MAGIC_MENUS_TITLE',"Magic menus");
+jr_define('_JOMRES_MAGIC_MENUS_MENUOPTIONS',"Add this search");
+jr_define('_JOMRES_MAGIC_MENUS_INFO',"To add this search to your Jomres Main Menu please give it a title and click the Save button. When that is done you can use the Access Control feature to enable/disable the new search option.");
+jr_define('_JOMRES_MAGIC_MENUS_SAVE',"Save search");
+jr_define('_JOMRES_MAGIC_MENUS_SEARCHTITLE',"Search title");
+jr_define('_JOMRES_MAGIC_MENUS_RAW',"Raw url");
+jr_define('_JOMRES_MAGIC_MENUS_CUSTOMSEARCHES',"Custom searches");
+jr_define('_JOMRES_MAGIC_MENUS_MENU_SECTION',"Menu section");
+jr_define('_JOMRES_MAGIC_MENUS_MENU_SECTION_INFO',"Optional, if you leave this blank then the new menu option will be added to the \"".jr_gettext('_JOMRES_MAGIC_MENUS_CUSTOMSEARCHES','_JOMRES_MAGIC_MENUS_CUSTOMSEARCHES',false)."\" menu section.");
+
---
+++ magic_menu/language/fr-FR.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 29/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_MAGIC_MENUS_TITLE',"Menus Magiques");
+jr_define('_JOMRES_MAGIC_MENUS_MENUOPTIONS',"Ajouter cette recherche ");
+jr_define('_JOMRES_MAGIC_MENUS_INFO',"Pour ajouter cette recherche à votre menu principal Jomres, donnez-lui un titre et cliquez sur le bouton Enregistrer. Lorsque cela est fait, vous pouvez utiliser la fonction de Contrôle d'accès pour activer/désactiver la nouvelle option de recherche.");
+jr_define('_JOMRES_MAGIC_MENUS_SAVE',"Enregister la recherche");
+jr_define('_JOMRES_MAGIC_MENUS_SEARCHTITLE',"Titre de la recherche");
+jr_define('_JOMRES_MAGIC_MENUS_RAW',"URL");
+jr_define('_JOMRES_MAGIC_MENUS_CUSTOMSEARCHES',"Recherches personnalisées");
+jr_define('_JOMRES_MAGIC_MENUS_MENU_SECTION',"Section du menu");
+jr_define('_JOMRES_MAGIC_MENUS_MENU_SECTION_INFO',"Facultatif, si vous laissez ce champ vide, la nouvelle option de menu sera ajoutée au fichier .jr_gettext ('_ JOMRES_MAGIC_MENUS_CUSTOMSEARCHES', '_ JOMRES_MAGIC_MENUS_CUSTOMSEARCHES', false). dans la section du menu.");
+
