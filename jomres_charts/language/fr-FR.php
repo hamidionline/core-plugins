c547191d4ace1bb4d883c078adeae1a39b9d4dcf
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 21:17:53 -0700

    Add files via upload

---
+++ jomres_charts/language/fr-FR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin
+ * @author Vince Wooll <sales@jomres.net>
+ * @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2010 Vince Wooll
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project, and use it accordingly, however all images, css and javascript which are copyright Vince Wooll are not GPL licensed and are not freely distributable. 
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( '_JOMRES_CHART_OCCUPANCY_DESC', 'Chambres réservées par jour/mois' );
+jr_define( '_JOMRES_HPROPERTY_VISITS_DESC', 'Nombre de visites par mois/année' );
+jr_define( '_JOMRES_HGUESTS_COUNTRY_DESC', 'Nombre de clients par pays' );
