a3c3eecfaeb70e88f606c5067ccdcf2daaf9d5f4
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 22:45:29 -0700

    Add files via upload

---
+++ logo_uploader/language/en-GB.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define( '_JOMRES_MEDIA_CENTRE_UPLOAD_CONTEXT_LOGO', 'Logo');
+jr_define( '_JOMRES_MEDIA_CENTRE_UPLOAD_CONTEXT_PROPERTY_LOGO', 'Property Logo');
---
+++ logo_uploader/language/fr-FR.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 29/06/2019 - https://www.valtari
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define( '_JOMRES_MEDIA_CENTRE_UPLOAD_CONTEXT_LOGO', 'Logo');
+jr_define( '_JOMRES_MEDIA_CENTRE_UPLOAD_CONTEXT_PROPERTY_LOGO', 'Logo de l\'établissement');
