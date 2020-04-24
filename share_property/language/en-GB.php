0a70eab3f4f3cec76547a0a246b0482b1ea8507f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 19:06:39 -0700

    Add files via upload

---
+++ share_property/language/en-GB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_SHARE_PROPERTY_TITLE',"Share Property");
+jr_define('_JRPORTAL_SHARE_PROPERTY_TITLE_SETTINGS',"Social Sharing");
+jr_define('_JRPORTAL_SHARE_PROPERTY_INSTRUCTIONS',"Select the social buttons you want to display on your property list and property details pages and if you want to use the long or short url in the shared link.<br />CURL must be available on your server if you want to use short urls.");
+jr_define('_JRPORTAL_SHARE_PROPERTY_HSTYLE',"Buttons style");
+jr_define('_JRPORTAL_SHARE_PROPERTY_HSHORTURL',"Use short url? (tiny.url is used)");
---
+++ share_property/language/fr-FR.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 30/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_SHARE_PROPERTY_TITLE',"Partager notre établissemnt avec votre communauté");
+jr_define('_JRPORTAL_SHARE_PROPERTY_TITLE_SETTINGS',"Partage social");
+jr_define('_JRPORTAL_SHARE_PROPERTY_INSTRUCTIONS',"Sélectionnez les boutons sociaux que vous souhaitez afficher sur votre liste d'établissement et sur les pages d'établissements, et si vous souhaitez utiliser l'URL longue ou courte dans le lien partagé.<br/> CURL doit être disponible sur votre serveur, si vous souhaitez utiliser des URL courtes.");
+jr_define('_JRPORTAL_SHARE_PROPERTY_HSTYLE',"Style des boutons");
+jr_define('_JRPORTAL_SHARE_PROPERTY_HSHORTURL',"Utiliser une URL courte? (tiny.url est utilisé)");
