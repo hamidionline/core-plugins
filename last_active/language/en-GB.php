45f7efa2c0d1c4376e407024d203430d35815eda
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 21:26:52 -0700

    Add files via upload

---
+++ last_active/language/en-GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_LATEST_BOOKINGS', 'Latest bookings');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_SINCE', 'Since you were online ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_PRETEXT_SINGULAR', 'There has been ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_PRETEXT_PLURAL', 'There have been ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_POSTTEXT_SINGULAR', ' booking for ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_POSTTEXT_PLURAL', ' bookings for ');
---
+++ last_active/language/fr-FR.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_LATEST_BOOKINGS', 'Dernières réservations');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_SINCE', 'Depuis que vous êtes en ligne ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_PRETEXT_SINGULAR', 'Il y a eu ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_PRETEXT_PLURAL', 'Il y a eu ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_POSTTEXT_SINGULAR', ' réservation de ');
+jr_define('_JOMRES_SINCE_LAST_ONLINE_POSTTEXT_PLURAL', ' réservations de ');
