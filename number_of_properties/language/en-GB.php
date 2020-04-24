8a9d2513138d28f48064c257c19dcb3e71e17e09
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 23:10:14 -0700

    Add files via upload

---
+++ number_of_properties/language/en-GB.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_NUMBER_OF_PROPERTIES_FOREWORD'," : ");
+jr_define('_NUMBER_OF_PROPERTIES_AFTERWORD'," hotels available worldwide. ");
+jr_define('_NUMBER_OF_PROPERTIES_BOOK_NOW',"Book your holiday online now!");
