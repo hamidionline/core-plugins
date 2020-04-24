9d86821b953def62d0df3d1fa1f959f8e92e696f
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 21:22:01 -0700

    Add files via upload

---
+++ jomres_selfregister_asamodule/language/en-GB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_SELFREGISTER_ASAMODULE_LINK','Click here to register your property');
+jr_define('_JOMRES_SELFREGISTER_ASAMODULE_BEFORELINK','');
+jr_define('_JOMRES_SELFREGISTER_ASAMODULE_AFTERLINK','');
