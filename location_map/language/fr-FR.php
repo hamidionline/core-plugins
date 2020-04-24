dd30c60381f2b69ac56f1c098e8e3a5433fafba6
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 21:51:00 -0700

    Add files via upload

---
+++ location_map/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 27/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+/**
+* @ignore
+*/
+jr_define('_JOMRES_LOCATION_MAP',"Liste des pays");
