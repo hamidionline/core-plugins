afefb3118312493ece647955a6776f00737535f1
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 22:47:43 -0700

    Add files via upload

---
+++ lucky_dip/language/en-GB.php
@@ -0,0 +1,30 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP',"Lucky dip");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_INFO',"Please enter the amount you would like to spend on your stay, choose a few features you're interested in, and let's see what we can find for you.");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST',"How much do you want to spend on your accommodation?");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_PLACEHOLDER',"E.G. 500");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_YOUENTERED',"You entered ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_NO_COST',"You didn't enter a sum, so we've entered one for you.");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_NO_RESULTS_COST',"Sorry, we couldn't find any properties that matched the sum you wanted to spend.");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_1',"For the sum you entered, you could stay at ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_2'," for ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_SINGULAR'," night. ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_PLURAL'," nights. ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_RELEVANT_FEATURES'," Relevant features : ");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_NOTE_GUESSTIMATE',"Note that these figures are rough guesses. Until you actually progress to the booking form the full price of your booking cannot be figured out, the prices listed here are only guidelines.");
---
+++ lucky_dip/language/fr-FR.php
@@ -0,0 +1,31 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 29/06/2019 - https://www.valtari
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP',"Lucky dip (moteur de recherche par budget)");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_INFO',"Please enter the amount you would like to spend on your stay, choose a few features you're interested in, and let's see what we can find for you.");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST',"How much do you want to spend on your accommodation?");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_PLACEHOLDER',"Par exemple : 500");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_YOUENTERED',"Vous avez renseigné ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_NO_COST',"Vous n'avez pas entré de somme, alors nous en avons entrée une à votre place.");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_COST_NO_RESULTS_COST',"Désolé, nous n'avons trouvé aucun établissement correspondant à la somme que vous souhaitez dépenser.");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_1',"Pour la somme que vous avez entrée, vous pouvez réserver à ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_2'," pur ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_SINGULAR'," nuitée. ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_PLURAL'," nuitées. ");
+jr_define('_JOMRES_TAGS_LUCKY_DIP_RESULT_RELEVANT_FEATURES'," Activités/équipement associés : ");
+
+jr_define('_JOMRES_TAGS_LUCKY_DIP_NOTE_GUESSTIMATE',"Notez que ces chiffres sont approximatifs. Tant que vous n'avez pas rempli un formulaire de réservation, le prix total de votre réservation ne peut pas être calculé, les prix ne sont qu'indicatifs.");
