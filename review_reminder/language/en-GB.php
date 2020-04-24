60c198eb98dbce9e8663a96411e9cc94d136190b
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 19:50:11 -0700

    Add files via upload

---
+++ review_reminder/language/en-GB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_REVIEW_REMINDER_TITLE',"Review reminders");
+jr_define('_JRPORTAL_REVIEW_REMINDER_NR_DAYS_TITLE1',"Send review reminder email..");
+jr_define('_JRPORTAL_REVIEW_REMINDER_NR_DAYS_TITLE2',"..days after the departure date");
+jr_define('_JRPORTAL_REVIEW_REMINDER_INSTRUCTIONS',"This plugin runs automatically in the background and sends a reminder email to guests to leave a review after a number of days from their departure date.");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_TITLE',"Please leave a review for ");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_MSGBODY1',"Thank you for choosing to stay with us. We hope you had a wonderful stay.<br /><br />We would like to have your opinion about the booked accommodation and the services we offered. It will take just a couple of minutes to complete it.<br /><br />Please click on the link ");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_LINK_TITLE',"Write a Review");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_MSGBODY2',"<br /><br />Our aim is to improve the quality of our services.<br /><br />Regards,<br />");
---
+++ review_reminder/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2011 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 30/06/2019 - https://www.valtari
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_REVIEW_REMINDER_TITLE',"Rappel d'avis");
+jr_define('_JRPORTAL_REVIEW_REMINDER_NR_DAYS_TITLE1',"Envoyer un mail de rappel d'avis ");
+jr_define('_JRPORTAL_REVIEW_REMINDER_NR_DAYS_TITLE2'," jour(s) après la date de départ");
+jr_define('_JRPORTAL_REVIEW_REMINDER_INSTRUCTIONS',"Ce plugin s'exécute automatiquement en arrière-plan et envoie un mail de rappel aux clients afin qu'ils laissent un commentaire/avis après un certain nombre de jours à compter de leur date de départ.");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_TITLE',"Please leave a review for ");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_MSGBODY1',"Merci d'avoir choisi notre établissement ! Nous espérons que vous avez passé un excellent séjour. <br /> <br /> Nous aimerions connaître votre opinion sur l’hébergement que vous avez réservé et sur les services que nous avons proposés. Il ne vous faudra que quelques minutes pour le remplir <br/> <br/> Veuillez cliquer sur le lien. ");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_LINK_TITLE',"Rédiger un avis");
+jr_define('_JOMRES_REVIEW_REMINDER_EMAIL_MSGBODY2',"<br/><br/>Notre objectif est d’améliorer la qualité de nos services, en prenant en compte vos avis.<br/><br/> Bien cordialement.<br/>");
