4f8e4113a2b6631455314e2e1aaa398db6289f60
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-23 17:03:23 -0700

    Add files via upload

---
+++ embed_video/language/en-GB.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_EMBED_VIDEO_TITLE',"Embed Video");
+jr_define('_JRPORTAL_EMBED_VIDEO_TITLE_TAB',"Video");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_YESNO',"Enabled? ");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_YES',"Yes");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_NO',"No");
+jr_define('_JRPORTAL_EMBED_VIDEO_WIDTH',"Width ");
+jr_define('_JRPORTAL_EMBED_VIDEO_HEIGHT',"Height ");
+jr_define('_JRPORTAL_EMBED_VIDEO_INSTRUCTIONS_FRONTEND',"Youtube video URL ");
+
+jr_define('_JRPORTAL_EMBED_VIDEO_ADVICE',"You should use the Embed url to your video, so if the url to the video in the browser is https://www.youtube.com/watch?v=XXXXXXXXX or https://youtu.be/XXXXXXXXX make sure you save the url as http://www.youtube.com/embed/XXXXXXXXX ");
---
+++ embed_video/language/fr-FR.php
@@ -0,0 +1,21 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+**/
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define('_JRPORTAL_EMBED_VIDEO_TITLE',"Intégrer une vidéo");
+jr_define('_JRPORTAL_EMBED_VIDEO_TITLE_TAB',"Vidéo");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_YESNO',"Activer ? ");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_YES',"Oui");
+jr_define('_JRPORTAL_EMBED_VIDEO_ENABLED_NO',"Non");
+jr_define('_JRPORTAL_EMBED_VIDEO_WIDTH',"Largeur ");
+jr_define('_JRPORTAL_EMBED_VIDEO_HEIGHT',"Hauteur ");
+jr_define('_JRPORTAL_EMBED_VIDEO_INSTRUCTIONS_FRONTEND',"URL Youtube de la vidéo ");
+
+jr_define('_JRPORTAL_EMBED_VIDEO_ADVICE',"Vous devez utiliser l'URL d'intégration de votre vidéo. Par conséquent, si l'URL de la vidéo dans le navigateur est https://www.youtube.com/watch?v=XXXXXXXXX ou https://youtu.be/XXXXXXXXX, veillez à enregistrer l'URL comme http://www.youtube.com/embed/XXXXXXXXX");
