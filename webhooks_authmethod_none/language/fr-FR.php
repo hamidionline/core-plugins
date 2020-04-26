3c2a20ec2be896862b5b3cba14169238e2cecab5
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-25 20:12:28 -0700

    Add files via upload

---
+++ webhooks_authmethod_none/language/fr-FR.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( 'WEBHOOKS_AUTH_METHOD_NONE', 'Aucun' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_NONE_NOTES', 'Utilisez cette méthode d\'intégration, si le service distant ne nécessite aucune information d\'identification de connexion.' );
+
