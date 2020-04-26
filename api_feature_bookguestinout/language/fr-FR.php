c7ed567c8c870efdbf94613117e14c9379c192b0
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-25 21:37:18 -0700

    Add files via upload

---
+++ api_feature_bookguestinout/language/fr-FR.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author  John m_majma@yahoo.com
+* @version Jomres 9 
+* @package Jomres
+* @copyright 2017
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die();
+// ################################################################
+
+jr_define('_OAUTH_SCOPE_PROPERTY_GET',"Obtenir l'établissement");
+jr_define('_OAUTH_SCOPE_PROPERTY_GET_DESC',"Le client peut récupérer des informations sur votre établissement.");
+
+jr_define('_OAUTH_SCOPE_PROPERTY_SET',"Paramétrer l'établissement");
+jr_define('_OAUTH_SCOPE_PROPERTY_SET_DESC',"Le client peut modifier votre (vos) établissement(s).");
+
+
+jr_define('_JOMRES_BOOKGUESTINOUT_API_CHECKIN',"Arrivé client");
+jr_define('_JOMRES_BOOKGUESTINOUT_API_UNDO_CHECKIN',"Annuler l'arrivé client");
+jr_define('_JOMRES_BOOKGUESTINOUT_API_CHECKOUT',"Départ client");
+jr_define('_JOMRES_BOOKGUESTINOUT_API_UNDO_CHECKOUT',"Annuler le départ client");
