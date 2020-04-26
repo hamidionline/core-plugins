96c2e67b2ed7872e7b7877c73d98e4409928fd65
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-25 20:15:18 -0700

    Add files via upload

---
+++ webhooks_authmethod_oauth/language/en-GB.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2', 'Oauth2' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_CLIENT_ID', 'Client ID' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_SECRET', 'Secret' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_NOTES', 'Use this integration method if the remote service requires login via OAuth2 Client Credentials.' );
---
+++ webhooks_authmethod_oauth/language/fr-FR.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2016 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+// ################################################################
+
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2', 'Oauth2' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_CLIENT_ID', 'ID client' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_SECRET', 'Secret' );
+jr_define( 'WEBHOOKS_AUTH_METHOD_OAUTH2_NOTES', 'Utilisez cette méthode d\'intégration si le service distant nécessite une connexion via les informations d\'identification du client OAuth2.' );
