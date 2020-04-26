7bf373dae760045324b08e442ec43d5cb869d663
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-25 22:11:59 -0700

    Add files via upload

---
+++ api_feature_charts/language/en-GB.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author  
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die();
+// ################################################################
+
+jr_define('_OAUTH_SCOPE_CHARTS_GET',"Get property");
+jr_define('_OAUTH_SCOPE_CHARTS_GET_DESC',"Client can retrieve information about charts of your property(s).");
+
+jr_define('_OAUTH_SCOPE_CHARTS_SET',"Set property");
+jr_define('_OAUTH_SCOPE_CHARTS_SET_DESC',"Client can modify charts of your property(s).");
---
+++ api_feature_charts/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author  
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die();
+// ################################################################
+
+jr_define('_OAUTH_SCOPE_CHARTS_GET',"Obtenir l'établissement");
+jr_define('_OAUTH_SCOPE_CHARTS_GET_DESC',"Le client peut récupérer des informations sur les graphiques/classements de votre(vos) établissement(s).");
+
+jr_define('_OAUTH_SCOPE_CHARTS_SET',"Paramétrer l'établissement");
+jr_define('_OAUTH_SCOPE_CHARTS_SET_DESC',"Le client peut modifier des informations sur les graphiques/classements de votre(vos) établissement(s).");
