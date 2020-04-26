8986519ed7fc13158653026bcd091766065d8440
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-25 20:53:37 -0700

    Add files via upload

---
+++ api_feature_blackbookings/language/fr-FR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+* Jomres CMS Agnostic Plugin
+* @author Woollyinwales IT <sales@jomres.net>
+* @version Jomres 9 
+* @package Jomres
+* @copyright	2005-2015 Woollyinwales IT
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 01/07/2019 - https://www.valtari.fr
+* Jomres (tm) PHP files are released under both MIT and GPL2 licenses. This means that you can choose the license that best suits your project.
+**/
+
+// ################################################################
+defined( '_JOMRES_INITCHECK' ) or die();
+// ################################################################
+
+jr_define('_OAUTH_SCOPE_PROPERTY_GET',"Obtenir l'établissement");
+jr_define('_OAUTH_SCOPE_PROPERTY_GET_DESC',"Le client peut récupérer des informations sur votre (vos) établissements.");
+
+jr_define('_OAUTH_SCOPE_PROPERTY_SET',"Paramétrer l'établissement");
+jr_define('_OAUTH_SCOPE_PROPERTY_SET_DESC',"Le client peut modifier votre (vos) établissement(s).");
