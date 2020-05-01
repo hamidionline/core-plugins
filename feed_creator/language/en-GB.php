c0a9087ee404c64426f9a66b3045190b148bb34c
Mark Errington <55329315+lopezacuba@users.noreply.github.com>
2020-04-30 20:34:34 -0700

    Add files via upload

---
+++ feed_creator/language/en-GB.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define('_JRPORTAL_FEED_CREATOR_TITLE',"RSS Feed");
+jr_define('_JRPORTAL_FEED_CREATOR_INSTRUCTIONS',"This plugin creates a feed from all jomres propertys. Currently RSS 1.0 and RSS 2.0 syndication formats are supported.");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDNAME',"Feed name");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDDESC',"Feed description");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDFILENAME',"Feed filename (no spaces)");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDFORMAT',"Syndication format");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDCACHETIME',"Cache time (in seconds)");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDIMAGEURL',"Feed image URL");
+jr_define('_JRPORTAL_FEED_CREATOR_HTRUNCATEDESC',"Truncate property description?");
+jr_define('_JRPORTAL_FEED_CREATOR_HTRUNCATEDESCSIZE',"Truncate after (chars)");
+jr_define('_JRPORTAL_FEED_CREATOR_HSHOWFEEDIMAGE',"Show feed image?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYIMAGE',"Show property image?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYTOWN',"Show property town?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYREGION',"Show property region?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYCOUNTRY',"Show property country?");
+jr_define('_JRPORTAL_FEED_CREATOR_HITEMS',"Limit number of syndicated propertys to");
---
+++ feed_creator/language/fr-FR.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* @license GNU/GPL http://www.gnu.org/copyleft/gpl.html
+* @author Aladar Barthi <sales@jomres-extras.com>
+* @copyright 2009-2010 Aladar Barthi
+* Translation to fr-FR by Stéphane Bernard, Valtari NumAgency, France - 25/06/2019 - https://www.valtari.fr
+**/
+##################################################################
+defined( '_JOMRES_INITCHECK' ) or die( '' );
+##################################################################
+
+jr_define('_JRPORTAL_FEED_CREATOR_TITLE',"Flux RSS");
+jr_define('_JRPORTAL_FEED_CREATOR_INSTRUCTIONS',"TCe plugin crée un flux de toutes les établissements de Jomres. Actuellement, les formats de syndication RSS 1.0 et RSS 2.0 sont pris en charge.");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDNAME',"Nom du flux");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDDESC',"Description du flux");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDFILENAME',"Nom du fichier du flux (pas d'espace)");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDFORMAT',"Format de syndication");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDCACHETIME',"Durée du cache (en secondes)");
+jr_define('_JRPORTAL_FEED_CREATOR_HFEEDIMAGEURL',"URL du flux d'images");
+jr_define('_JRPORTAL_FEED_CREATOR_HTRUNCATEDESC',"Réduire la description de l'établissement ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HTRUNCATEDESCSIZE',"Réduire après (x caractères)");
+jr_define('_JRPORTAL_FEED_CREATOR_HSHOWFEEDIMAGE',"Afficher l'image du flux ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYIMAGE',"Afficher l'image de l'établissement ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYTOWN',"Afficher la ville de l'établissement ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYREGION',"Afficher la région de l'établissement ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HPROPERTYCOUNTRY',"Affciher le pays de l'établissement ?");
+jr_define('_JRPORTAL_FEED_CREATOR_HITEMS',"Limiter le nombre d'établissement(s) souscrits à ");
